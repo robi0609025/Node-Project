bd5287d31d5daa726e6c4b4524954fa6f0533d13
robi0609025 <robi.kuet@gmail.com>
2020-01-22 17:27:09 +1100

    Update app.js
    Learning few request properties

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const http = require('http');
 
 //another way to create server - using arrow function
 const server = http.createServer((req, res)=> {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    //process.exit();  //to shut down the listener
 });
 server.listen(3000);
