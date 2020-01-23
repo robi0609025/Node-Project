932512091a4967e4fb6af2564dda69d99024862b
robi0609025 <robi.kuet@gmail.com>
2020-01-23 15:33:02 +1100

    Update app.js - Parsing data bodies
    Parsing data from POST method and save it to the local file

--- app.js
+++ app.js
@@ -10,17 +10,31 @@ const server = http.createServer((req, res)=> {
     //console.log(req.url, req.method, req.headers);
     const url = req.url;
     const method = req.method;
+    
 
     if(url === '/'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Enter Message!!!</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     if (url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'Dummy');
+        const body = [];
+        
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        //fs.writeFileSync('message.txt', 'Dummy');
         // res.writeHead(302, {});   
         res.statusCode = 302; 
         res.setHeader('Location', '/');
@@ -32,9 +46,6 @@ const server = http.createServer((req, res)=> {
     res.write('<body><h1>Hello from Node js server.</h1></body>');
     res.write('</html>');
     res.end();
-    
-
-
 });
 
 server.listen(3000);
